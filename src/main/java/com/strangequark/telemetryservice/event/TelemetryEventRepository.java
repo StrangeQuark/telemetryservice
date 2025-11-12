c754b31d784f965e6661bab866986dca54891b71
StrangeQuark <zacharyzirkle@hotmail.com>
2025-11-11 21:12:24 -0600

    Add first telemetry data retrieval endpoint

--- src/main/java/com/strangequark/telemetryservice/event/TelemetryEventRepository.java
+++ src/main/java/com/strangequark/telemetryservice/event/TelemetryEventRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TelemetryEventRepository extends MongoRepository<TelemetryEvent, String> {
+public interface TelemetryEventRepository extends MongoRepository<TelemetryEvent, String>, TelemetryEventRepositoryWrapper {
+
 }
